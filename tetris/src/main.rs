aedeb153d0296f5a7c4fe605e6e9ccafacb1de44
Vladdimir Vankov <vladimirvankovvankov@gmail.com>
2024-12-01 10:03:06 +0200

    start implementing Tetris game

---
+++ tetris/src/main.rs
@@ -0,0 +1,52 @@
+
+extern crate sdl2;
+
+use sdl2::clear_error;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::render::{Canvas, Texture, TextureAccess, TextureCreator};
+use sdl2::rect::Rect;
+use std::sync::mpsc::RecvError;
+use std::time::Duration;
+use std::thread::sleep;
+
+const TEXTURE_SIZE:u32 = 32;
+
+fn main() {
+    let sdl_context = sdl2::init().expect("SDL Initialisation failed");
+    let video_subsystem = sdl_context.video().expect("Couldn't get SDL video subsystem");
+    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+    .position_centered()
+    .opengl()
+    .build()
+    .expect("Failed to create window");
+
+    let mut canvas = window.into_canvas().build().expect("Failed to convert window into canvas");
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+    let mut square_texture: Texture = texture_creator.create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE).expect("Failed to create texture");
+    canvas.with_texture_canvas(&mut square_texture, |texture|{
+        texture.set_draw_color(Color::RGB(0, 255, 0));
+        texture.clear();
+    });
+    
+    let mut event_pump = sdl_context.event_pump().expect("Failed to get SDL event pump");
+    'running: loop{
+        for event in event_pump.poll_iter() {
+            match  event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => 
+                {
+                    break 'running;
+                },
+                _ => {}    
+            }
+        }
+
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
+        canvas.clear();
+        canvas.copy(&square_texture, None, Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE)).expect("Failed to copy texture in to canvas");
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
